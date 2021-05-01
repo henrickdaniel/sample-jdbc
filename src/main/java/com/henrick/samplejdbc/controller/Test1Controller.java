e150e725d58490f01e1bc9de8f050b48809d50b3
henrick.soares <henrickdaniel@gmail.com>
2021-05-01 15:38:15 -0300

    Testing config vars

--- src/main/java/com/henrick/samplejdbc/controller/Test1Controller.java
+++ src/main/java/com/henrick/samplejdbc/controller/Test1Controller.java
@@ -16,6 +16,11 @@ public class Test1Controller {
     public ResponseEntity test(){
         List<CostumerVO> costumers = new ArrayList<>();
         costumers.add(new CostumerVO(1, "Henrick Daniel Soares"));
+        final String DATABASE_URL = "CLEARDB_DATABASE_URL";
+
+        System.out.println("ENV...: " + System.getenv(DATABASE_URL));
+        System.out.println("PRO...: " + System.getProperty(DATABASE_URL));
+
         return new ResponseEntity<List<CostumerVO>>(costumers, HttpStatus.OK);
     }
 
